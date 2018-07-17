369d60a636cdc26615d307dd36ce2fdb45047c84
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-17 06:01:17 +0000

    local copy of PinMap.h 	new file:   source/PinMap_local.h

---
+++ source/PinMap_local.h
@@ -0,0 +1,15 @@
+#ifndef PIN_MAP_H
+#define PIN_MAP_H
+
+#include "sam.h"
+#include "SeesawConfig.h"
+
+#define PORTA 0
+#define PORTB 1
+#define PORTC 2
+#define NO_PORT 3
+
+#define NOT_ON_TC 0
+#define NOT_ON_TCC 0
+
+#endif

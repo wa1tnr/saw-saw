27acadd00aae1d6453ad0fb1cc24588610dabb95
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-17 03:01:06 +0000

    Nice NeoPixel demo - bare metal (almost/sort-of/not-quite) 	modified:   source/main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,10 @@
+// Tue 17 Jul 02:58:18 UTC 2018
+// main.cpp
+
 /*
- * main.cpp
- *
  * Created: 7/17/2017 3:26:42 PM
  * Author : deanm
  */ 
-
 
 #include "sam.h"
 #include "board_init.h"
@@ -30,6 +30,9 @@
 #include "Neopixel.h"
 
 #include "bsp_gpio.h"
+#if CONFIG_POWER_SENSE
+#include "bsp_neopix.h"
+#endif
 
 using namespace QP;
 
@@ -89,11 +92,59 @@ static AODAP dap;
 static Neopixel neopixel;
 #endif
 
+void uSec(void) {
+    for (volatile int i = 1; i < 2; i++) { // needs calibration
+        // nothing
+    }
+}
+
+void wide_timer(void) { // human blinkie timescale
+    uint32_t on_time  = 2140111222; // it's 2147 something ;)
+    for(on_time = 2140111; on_time > 0; on_time--) {
+        uSec();
+    }
+}
+
+void init_neopixels(void) {
+    gpio_init(PORTA, CONFIG_POWER_SENSE_NEOPIX_PIN, 1); // 27
+}
+
+uint32_t color = 0;
+
+void to_color(void) {
+    // gpio_init(PORTA, CONFIG_POWER_SENSE_NEOPIX_PIN, 1);
+    neopix_show_800k(CONFIG_POWER_SENSE_NEOPIX_PIN, (uint8_t *)&color, 4);
+    wide_timer();
+}
+
+void neo_pixel_demo(void) {
+    init_neopixels();
+    //        BBRRGG
+    color = 0x090000;    // blue   //
+    to_color();
+    wide_timer(); wide_timer(); wide_timer();
+    color = 0x090900;    // magenta //
+    to_color();
+    color = 0x000009;    // green  //
+    to_color();
+    color = 0x090000;    // blue   //
+    to_color();
+    color = 0x000900;    // red    //
+    to_color();
+    color = 0x001903;    // orange //
+    to_color();
+}
+
 int main(void)
 {
     /* Initialize the SAM system */
     SystemInit();
-	board_init();
+        board_init();
+        neo_pixel_demo();
+        while(1); // ////////////  trap!  ////////////////
+}
+
+/*      // big comment-out block AA:
 	
 	QF::init();
 	QF::poolInit(evtPoolSmall, sizeof(evtPoolSmall), EVT_SIZE_SMALL);
@@ -168,3 +219,5 @@ int main(void)
 	
 	QP::QF::run();
 }
+
+*/
